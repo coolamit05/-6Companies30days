ece2211f343106f51fedba50c593f9eb118dd574
Amit Kumar <118215968+coolamit05@users.noreply.github.com>
2025-01-04 08:26:37 +0530

    Time: 3 ms (59.69%) | Memory: 61.2 MB (10.88%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -3,14 +3,7 @@ class Solution
     public int waysToSplitArray(int[] nums) 
     {
         int n = nums.length;
-        long[] prefArray = new long[n]; // Use long to handle large sums
         long[] sufArray = new long[n];
-
-        // Calculate prefix sum
-        prefArray[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefArray[i] = prefArray[i - 1] + nums[i];
-        }
 
         // Calculate suffix sum
         sufArray[n - 1] = nums[n - 1];
@@ -20,8 +13,10 @@ class Solution
 
         // Count the number of valid splits
         int count = 0;
+        long prefSum=0;
         for (int i = 0; i < n - 1; i++) {
-            if (prefArray[i] >= sufArray[i + 1]) {
+            prefSum+=nums[i];
+            if (prefSum >= sufArray[i + 1]) {
                 count++;
             }
         }
